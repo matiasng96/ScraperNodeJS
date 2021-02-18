9870d53bb7c60b56ae4a3cd079cb40a66c4d1379
Matías Gallego <matiasngallego@gmail.com>
2021-02-17 22:41:28 -0300

    -

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const request = require('request-promise');
 const cheerio = require('cheerio');
 const mongoose = require('mongoose');
 const model = require('./models/teamStats');
-const db = 'mongodb+srv://matiasng:GnXBcKvAIobg2Zv9@cluster0.4i9ha.mongodb.net/test?retryWrites=true&w=majority';
 const db = process.env.MONGODB_URI;
 ////////////////////////////////////////////////
 ////////////////////////////////////////////////
